a4e61ff50f0273bc6412341a447f8122e22715c9
Amalendu Pandey <amalendupandey56@gmail.com>
2023-09-02 22:06:10 +0530

    server header full functionality done

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -2,6 +2,35 @@ import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server"
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile)
+      return new NextResponse(JSON.stringify({ error: "Unauthorized" }), {
+        status: 401,
+      });
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    });
+
+    return new NextResponse(JSON.stringify(server), { status: 200 });
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
+    return new NextResponse(
+      JSON.stringify({ error: "Internal Server Error" }),
+      { status: 500 }
+    );
+  }
+}
+
 export async function PATCH(
     req:Request,
     {params}:{params:{serverId:string}}
